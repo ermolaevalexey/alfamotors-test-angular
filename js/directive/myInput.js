e056b7fb39c1f504683f3d97b615102eff517414
Алексей Ермолаев <aleksejermolaev@MacBook-Pro-Aleksej.local>
2016-08-06 18:39:36 +0300

    set required attribute to needed inputs

--- js/directive/myInput.js
+++ js/directive/myInput.js
@@ -1,21 +1,25 @@
 'use strict';
 
 (function (w) {
-	
+
 	angular.module('app')
 
 	.directive('myInput', myInputDirective);
 
 	function myInputDirective() {
 		return {
-			require: '^myForm',
 			restrict: 'E',
 			transclude: true,
 			scope: {
 				name: '@name',
 				type: '@type',
-				required: '@required',
 				label: '@label',
+			},
+			link: function (scope, el, attrs) {
+				var input = el.find('input');
+				if (attrs.required !== undefined) {
+					input.attr('required', true);
+				}
 			},
 			templateUrl: '/js/partials/my-input.html'
 		}
