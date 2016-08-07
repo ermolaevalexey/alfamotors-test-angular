bce847a36a910beb157c45639437fef12cd7ff63
Алексей Ермолаев <aleksejermolaev@MacBook-Pro-Aleksej.local>
2016-08-07 18:33:16 +0300

    organized code better

--- js/directive/myInput.js
+++ js/directive/myInput.js
@@ -9,22 +9,17 @@
 			require: ['^myForm', 'ngModel'],
 			restrict: 'E',
 			transclude: true,
-			scope: true,
+			scope: {
+				name: '@name',
+				type: '@type',
+				label: '@label',
+				uiMask: '@uiMask'
+			},
 			link: function (scope, el, attrs, ctrl) {
 				var input = el.find('input');
-				console.log(el);
 				if (attrs.required !== undefined) {
 					input.attr('required', true);
 				}
-				scope.name = attrs.name;
-				scope.type = attrs.type;
-				scope.label = attrs.label;
-				scope.ngModel = attrs.ngModel;
-				scope.required = attrs.required !== undefined ? 'required' : '';
-				scope.uiMask = attrs.uiMask !== undefined ? attrs.uiMask : '';
-				scope.uiMaskPlaceholder = attrs.uiMaskPlaceholder !== undefined ? 'ui-mask-placeholder' : '';
-				scope.uiMaskPlaceholderChar = attrs.uiMaskPlaceholderChar !== undefined ? attrs.uiMaskPlaceholderChar : '';
-				scope.modelViewValue = attrs.modelViewValue !== undefined ? true : '';
 			},
 			templateUrl: '/js/partials/my-input.html'
 		}
